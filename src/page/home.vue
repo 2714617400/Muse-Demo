c85394cbd1ebfc40d57600912c2acbf6cabba3f3
Li <17774657825@163.com>
2020-03-30 23:33:23 +0800

    first commit

---
+++ src/page/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <mu-container class="button-wrapper">
+        <mu-button color="primary">Primary</mu-button>
+        <mu-button color="secondary">Secondary</mu-button>
+        <mu-button color="success">Success</mu-button>
+        <mu-button color="warning">Warning</mu-button>
+        <mu-button color="info">Info</mu-button>
+        <mu-button color="error">Error</mu-button>
+        <mu-button disabled>disabled</mu-button>
+      </mu-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.button-wrapper {
+  text-align: center;
+  .mu-button{
+    margin: 8px;
+  }
+}
+</style>

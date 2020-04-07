d934dad01c888491c6edb62d2f0453a41c19ab7b
何黎 <heli@helideMac-mini.local>
2020-04-07 19:28:21 +0800

    指定vue版本2.6.8

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import MuseUI from 'muse-ui'
+import Helpers from 'muse-ui/lib/Helpers'
 import 'muse-ui/dist/muse-ui.css'
 
 Vue.config.productionTip = false
 
 Vue.use(MuseUI)
+Vue.use(Helpers)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/page/home.vue
+++ src/page/home.vue
@@ -2,41 +2,43 @@
   <div class="hello">
     <mu-appbar style="width: 100%;" title="Title" color="primary" z-depth="2">
       <mu-avatar slot="right">
-        <img src="../assets/logo.png">
+        <img src="../assets/logo.png" />
       </mu-avatar>
     </mu-appbar>
     <h1>好久不见，MASTER</h1>
     <mu-container class="button-wrapper">
-        <mu-button color="primary">Primary</mu-button>
-        <mu-button color="secondary">Secondary</mu-button>
-        <mu-button color="success">Success</mu-button>
-        <mu-button color="warning">Warning</mu-button>
-        <mu-button color="info">Info</mu-button>
-        <mu-button color="error">Error</mu-button>
-        <mu-button disabled>disabled</mu-button>
-      </mu-container>
+      <mu-button color="primary">Primary</mu-button>
+      <mu-button color="secondary">Secondary</mu-button>
+      <mu-button color="success">Success</mu-button>
+      <mu-button color="warning">Warning</mu-button>
+      <mu-button color="info">Info</mu-button>
+      <mu-button color="error">Error</mu-button>
+      <mu-button disabled>disabled</mu-button>
+    </mu-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  data () {
+  name: "home",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
 h1 {
-  font-size: 96px; font-weight: 300; padding-left: 30px;
+  font-size: 96px;
+  font-weight: 300;
+  padding-left: 30px;
 }
 .button-wrapper {
   text-align: center;
-  .mu-button{
+  .mu-button {
     margin: 8px;
   }
 }
